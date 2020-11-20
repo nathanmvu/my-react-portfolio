3c9d6a1971c94187cd9c29531fdb36b65e83fa3f
Nathan Vu <nathanvu99@gmail.com>
2020-11-19 20:47:28 -0800

    Fixed routing issue

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -11,9 +11,9 @@ function App() {
     <Router>
       <Navbar />
       <Switch>
-        <Route exact path='/' component={About} />
-        <Route exact path='/portfolio' component={Portfolio} />
-        <Route exact path='/contact' component={Contact} />
+        <Route exact path={(process.env.PUBLIC_URL || '/') + '/'} component={About} />
+        <Route exact path={(process.env.PUBLIC_URL || '/') + '/portfolio'} component={Portfolio} />
+        <Route exact path={(process.env.PUBLIC_URL || '/') + '/contact'} component={Contact} />
       </Switch>
     </Router>
   );
--- portfolio/src/components/Navbar/Navbar.js
+++ portfolio/src/components/Navbar/Navbar.js
@@ -5,7 +5,7 @@ function Navbar() {
   return (
     <nav class="navbar navbar-expand-md navbar-light">
       <h1>
-        <a id='nameBadge' class="navbar-brand ml-5" href="/">Nathan Vu</a>
+        <a id='nameBadge' class="navbar-brand ml-5" href={(process.env.PUBLIC_URL || '/') + '/'}>Nathan Vu</a>
       </h1>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -13,16 +13,16 @@ function Navbar() {
       <div class="collapse navbar-collapse justify-content-end">
         <ul class="navbar-nav">
           <li class="nav-item">
-            <a class="nav-link" href="/">about</a>
+            <a class="nav-link" href={(process.env.PUBLIC_URL || '/') + '/'}>about</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="/portfolio">portfolio</a>
+            <a class="nav-link" href={(process.env.PUBLIC_URL || '/') + '/portfolio'}>portfolio</a>
           </li>
           <li class="nav-item">
             <a class="nav-link" target="_blank" href="./assets/img/NV-Resume-2020.pdf">resume</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="/contact">contact</a>
+            <a class="nav-link" href={(process.env.PUBLIC_URL || '/') + '/contact'}>contact</a>
           </li>
         </ul>
       </div>
